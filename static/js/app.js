fe94c81533a6b767dea214a1f1e722c2bf1e68b5
Tas <tas0173@gmail.com>
2020-09-02 13:20:50 +1000

    Wed Lunch commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,27 @@
+// 1. Use the D3 library to read in `samples.json`.
 
 // Wrap the reading in of the data into a function
-function readData(id) {
+// function readData(id) {
     
 // Use d3.json() to fetch data from JSON file
 // Incoming data is internally referred to as incomingData
     d3.json("data/samples.json").then((incomingData) => {
-        function filteredSampleID(samples) {
-        return samples.otu_ids;
-        }
-        function filteredSampleValues(samples) {
-        return samples.sample_values;
-        }
-        function filteredSampleLabels(samples) {
-        return samples.otu_labels;
-        }
+        console.log(incomingData)
+        // function filteredSampleID(samples) {
+        // return samples.otu_ids;
+        // }
+        // function filteredSampleValues(samples) {
+        // return samples.sample_values;
+        // }
+        // function filteredSampleLabels(samples) {
+        // return samples.otu_labels;
+        // }
 
-        var otu_ids = incomingData.filter(filteredSampleID);
-        console.log(out_ids);
+        // var otu_ids = incomingData.filter(filteredSampleID);
+        // console.log(out_ids);
+
+
 
     });
 
-}
+// }
