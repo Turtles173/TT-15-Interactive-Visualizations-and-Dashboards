d872253756fed503ff3929e6325d3ff21c09abaf
Tas <tas0173@gmail.com>
2020-09-03 18:15:13 +1000

    Update apptt.js

--- static/apptt.js
+++ static/apptt.js
@@ -1,13 +1,37 @@
 
 // Create a function to fetch data 
-function dataFetch(ids) {
+function dataFetch(id) {
 
 // Incoming data is internally referred to as sampleData
 
-    d3.json("/data/samples.json").then(function(sampleData) {
-      console.log(sampleData[0])
-      // var washFrequency = sampleData.metadata.map(data => data.wfreq)
-      // console.log(`Test: ${washFrequency}`)
+    d3.json("/data/samples.json").then(function(data) {
+      console.log(data[0])
+
+      // Grab the wash frequency (wfreq)
+      var washFrequency = sampleData.metadata.map(data => data.wfreq)
+      console.log(`Test: ${washFrequency}`)
+            
+      // Filter by ID
+      var sampleIDS = sampleData.sample.filter(sam => sam.id == id)[0]
+      console.log(sampleIDS);
+
+      // Slice to get the top 10 - sample values (sample_value)
+      
+
+
+
+      // Slice to get the top 10 - otu ids (out_ids)
+
+
+
+
+      // 
+
+
+
+
+
+
 
 
 
